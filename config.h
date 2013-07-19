1a06da7d36a04e9b1ba7b6b07dc80f17c6b1c306
bubylou <bubylou7@gmail.com>
2013-07-19 14:18:07 -0400

    mpdcontrol patch

--- config.h
+++ config.h
@@ -55,7 +55,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *termcmd[]  = { "x-terminal-emulator", NULL };
+
+#include "mpdcontrol.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,6 +96,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_F1,     mpdchange,      {.i = -1} },
+	{ MODKEY,                       XK_F2,     mpdchange,      {.i = +1} },
+	{ MODKEY,                       XK_Escape, mpdcontrol,     {0} },
 };
 
 /* button definitions */
